f064c2ea4e8a0cdf01f2161c7c66824abadc1662
ShokoMatsuda <syokom.0128@gmail.com>
2023-10-26 20:21:57 +0900

    クリック時にリストを追加してみようまで実装

--- js/event.js
+++ js/event.js
@@ -5,3 +5,19 @@ const btn = document.getElementById('output-btn');
 btn.addEventListener('click', () => {
     console.log('クリックされました！');
 });
+
+// add-btnというidを持つHTML要素を取得し、定数に代入する
+const addBtn = document.getElementById('add-btn');
+// parent-listというidを持つHTML要素を取得し、定数に代入する
+const parentList = document.getElementById('parent-list');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+addBtn.addEventListener('click', () => {
+    // li要素を新しく作成する
+    const childList = document.createElement('li');
+    // 作成したli要素に「これはリスト要素です」というテキストを追加する
+    childList.textContent = 'これはリスト要素です';
+
+    // ul要素の末尾にli要素を追加する
+    parentList.appendChild(childList);
+})
